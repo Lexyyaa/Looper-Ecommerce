585d32f33fc59c905175d76098c09ab4fe9c2c26
Lexyyaa <shw9910274@gmail.com>
2025-08-01 16:24:18 +0900

    feat : [상품] 상품관련 엔티티 및 테스트코드 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductSku.java
@@ -0,0 +1,63 @@
+package com.loopers.domain.product;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_sku")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProductSku extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "sku", nullable = false, unique = true)
+    private String sku;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(name = "stock_total", nullable = false)
+    private int stockTotal;
+
+    @Column(name = "stock_reserved", nullable = false)
+    private int stockReserved;
+
+    public static ProductSku create(Product product, String sku, int price, int stockTotal, int stockReserved) {
+        return ProductSku.builder()
+                .product(product)
+                .sku(sku)
+                .price(price)
+                .stockTotal(stockTotal)
+                .stockReserved(stockReserved)
+                .build();
+    }
+
+    public boolean isSoldOut() {
+        return (stockTotal - stockReserved) <= 0;
+    }
+
+    public int avaliableQunatity() {
+        return (stockTotal - stockReserved);
+    }
+
+    public void reserveStock(int quantity) {
+        if (avaliableQunatity() < quantity) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "재고가 부족합니다.");
+        }
+
+        stockReserved += quantity;
+    }
+}
+
