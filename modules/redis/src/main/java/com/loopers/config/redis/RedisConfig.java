774b6507a1a9d2f6abcdd4ee22b9281c95d16164
Lexyyaa <shw9910274@gmail.com>
2025-08-15 14:08:52 +0900

    chore : Redis 설정 (String, Object 로 변경)

--- modules/redis/src/main/java/com/loopers/config/redis/RedisConfig.java
+++ modules/redis/src/main/java/com/loopers/config/redis/RedisConfig.java
@@ -1,31 +1,64 @@
 package com.loopers.config.redis;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.lettuce.core.ReadFrom;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.NoOpCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.function.Consumer;
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
+@EnableCaching
+@RequiredArgsConstructor
 public class RedisConfig {
 
     public static final String CONNECTION_MASTER = "redisConnectionMaster";
     public static final String REDIS_TEMPLATE_MASTER = "redisTemplateMaster";
 
+    private boolean isCacheEnabled = true;
+
     private final RedisProperties redisProperties;
 
-    public RedisConfig(RedisProperties redisProperties) {
-        this.redisProperties = redisProperties;
+    private ObjectMapper createRedisObjectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        om.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.EVERYTHING,
+                JsonTypeInfo.As.PROPERTY
+        );
+        return om;
+    }
+
+    @Bean
+    public GenericJackson2JsonRedisSerializer redisJsonSerializer() {
+        return new GenericJackson2JsonRedisSerializer(createRedisObjectMapper());
     }
 
     @Primary
@@ -52,18 +85,62 @@ public class RedisConfig {
 
     @Primary
     @Bean
-    public RedisTemplate<String, String> defaultRedisTemplate(
-            LettuceConnectionFactory lettuceConnectionFactory
+    public RedisTemplate<String, Object> defaultRedisTemplate(
+            LettuceConnectionFactory lettuceConnectionFactory,
+            GenericJackson2JsonRedisSerializer redisJsonSerializer
     ) {
-        return configureRedisTemplate(new RedisTemplate<>(), lettuceConnectionFactory);
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(redisJsonSerializer);
+        template.setHashValueSerializer(redisJsonSerializer);
+
+        template.afterPropertiesSet();
+        return template;
     }
 
     @Qualifier(REDIS_TEMPLATE_MASTER)
     @Bean
-    public RedisTemplate<String, String> masterRedisTemplate(
-            @Qualifier(CONNECTION_MASTER) LettuceConnectionFactory lettuceConnectionFactory
+    public RedisTemplate<String, Object> masterRedisTemplate(
+            @Qualifier(CONNECTION_MASTER) LettuceConnectionFactory lettuceConnectionFactory,
+            GenericJackson2JsonRedisSerializer redisJsonSerializer
     ) {
-        return configureRedisTemplate(new RedisTemplate<>(), lettuceConnectionFactory);
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(redisJsonSerializer);
+        template.setHashValueSerializer(redisJsonSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Primary
+    @Bean
+    public CacheManager redisCacheManager(
+            @Qualifier("defaultRedisConnectionFactory") RedisConnectionFactory connectionFactory,
+            GenericJackson2JsonRedisSerializer redisJsonSerializer
+    ) {
+        if (!isCacheEnabled) {
+            return new NoOpCacheManager();
+        }
+
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisJsonSerializer))
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .transactionAware()
+                .build();
     }
 
     private LettuceConnectionFactory lettuceConnectionFactory(
@@ -72,28 +149,20 @@ public class RedisConfig {
             List<RedisNodeInfo> replicas,
             Consumer<LettuceClientConfiguration.LettuceClientConfigurationBuilder> customizer
     ) {
-        LettuceClientConfiguration.LettuceClientConfigurationBuilder builder = LettuceClientConfiguration.builder();
-        customizer.accept(builder);
-        LettuceClientConfiguration lettuceClientConfiguration = builder.build();
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientBuilder = LettuceClientConfiguration.builder();
+        customizer.accept(clientBuilder);
+        LettuceClientConfiguration clientConfig = clientBuilder.build();
 
-        RedisStaticMasterReplicaConfiguration masterReplicaConfig =
+        RedisStaticMasterReplicaConfiguration topology =
                 new RedisStaticMasterReplicaConfiguration(master.host(), master.port());
-        masterReplicaConfig.setDatabase(database);
+        topology.setDatabase(database);
 
-        replicas.forEach(replica -> masterReplicaConfig.addNode(replica.host(), replica.port()));
+        if (replicas != null) {
+            for (RedisNodeInfo replica : replicas) {
+                topology.addNode(replica.host(), replica.port());
+            }
+        }
 
-        return new LettuceConnectionFactory(masterReplicaConfig, lettuceClientConfiguration);
-    }
-
-    private <K, V> RedisTemplate<K, V> configureRedisTemplate(
-            RedisTemplate<K, V> template,
-            LettuceConnectionFactory connectionFactory
-    ) {
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-        return template;
+        return new LettuceConnectionFactory(topology, clientConfig);
     }
 }
