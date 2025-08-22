86bc7ce299e97ad01f3b27866836424f92041bb7
Lexyyaa <shw9910274@gmail.com>
2025-08-22 16:32:04 +0900

    feat : [결제] 결제요청 시 타입별 별개의 프로세서 적용

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentCommand.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentCommand.java
@@ -1,16 +1,75 @@
 package com.loopers.domain.payment;
 
+import com.loopers.interfaces.api.controller.payment.PaymentV1Request;
+import com.loopers.interfaces.api.controller.paymentgateway.PaymentGatewayV1Request;
+
+import java.math.BigDecimal;
+
 public class PaymentCommand {
     public record CreatePayment(
             String loginId,
             Long orderId,
             Long amount,
-            String method
+            Payment.Method method,
+            String idempotencyKey,
+            CardPaymentDetails details
+    ) {
+        public static CreatePayment create(
+                PaymentV1Request.CreatePayment request,
+                Long orderId
+        ) {
+            return new CreatePayment(
+                    request.loginId(),
+                    orderId,
+                    request.amount(),
+                    request.method(),
+                    request.idempotencyKey(),
+                    new CardPaymentDetails(
+                            request.details().cardType(),
+                            request.details().cardNo()
+                    )
+            );
+        }
+    }
+
+    //TODO. 카드결제 DTO 추후 추상체와 구현체 분리
+    public record CardPaymentDetails(
+            String cardType,
+            String cardNo
     ) {}
+
+    public record SyncPayment(
+            String transactionKey,
+            String orderId,
+            BigDecimal amount,
+            String status,
+            String reason
+    ){
+        public static SyncPayment create(PaymentGatewayV1Request.TransactionInfo request ) {
+            return new SyncPayment(
+                    request.transactionKey(),
+                    request.orderId(),
+                    request.amount(),
+                    request.status(),
+                    request.reason()
+            );
+        }
+
+        public static SyncPayment from(Payment payment) {
+            return new SyncPayment(
+                    payment.getTxKey(),
+                    payment.getOrderId().toString(),
+                    BigDecimal.valueOf(payment.getAmount()),
+                    payment.getStatus().toString(),
+                    payment.getFailReason()
+            );
+        }
+    }
 
     public record CancelPayment(
             String loginId,
             Long orderId,
             Long paymentId
     ) {}
+
 }
--- apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentInfo.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentInfo.java
@@ -3,6 +3,7 @@ package com.loopers.domain.payment;
 import java.time.ZonedDateTime;
 
 public class PaymentInfo {
+
     public record CreatePayment(
             Long paymentId,
             Long userId,
@@ -10,7 +11,9 @@ public class PaymentInfo {
             Long amount,
             Payment.Method method,
             Payment.Status status,
-            ZonedDateTime createdAt
+            String idempotencyKey,
+            String transactionKey,
+            ZonedDateTime updatedAt
     ) {
         public static CreatePayment from(Payment payment) {
             return new CreatePayment(
@@ -20,7 +23,9 @@ public class PaymentInfo {
                     payment.getAmount(),
                     payment.getMethod(),
                     payment.getStatus(),
-                    payment.getCreatedAt()
+                    payment.getIdempotencyKey(),
+                    payment.getTxKey(),
+                    payment.getUpdatedAt()
             );
         }
     }
