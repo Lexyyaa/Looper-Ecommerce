0f023d0495a7162de30964bb2fabfd851d8507df
Lexyyaa <shw9910274@gmail.com>
2025-08-22 16:44:16 +0900

    feat : [결제] PG연동 callback api

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/PaymentRepository.java
@@ -1,8 +1,12 @@
 package com.loopers.domain.payment;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository {
     Payment save(Payment payment);
     Optional<Payment> findById(Long id);
+    Optional<Payment> findByTxKey(String txKey);
+    List<Payment> findRecentWaiting(LocalDateTime since,Payment.Status status);
 }
