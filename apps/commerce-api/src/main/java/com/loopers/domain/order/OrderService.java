d6e6e8f2dc73d21771f3653b31e58c77206d1798
Lexyyaa <shw9910274@gmail.com>
2025-08-28 04:05:26 +0900

    fix : 불필요한 코드 삭제

--- apps/commerce-api/src/main/java/com/loopers/domain/order/OrderService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/OrderService.java
@@ -1,10 +1,10 @@
 package com.loopers.domain.order;
 
-import com.loopers.domain.user.User;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,28 +19,38 @@ public class OrderService {
     }
 
     public Order getOrder(Long orderId) {
-        return orderRepository.findById(orderId)
+        Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND,"존재하지 않는 주문입니다."));
+        order.isPending();
+        return order ;
+    }
+
+    @Transactional(readOnly = true)
+    public Order getPendingOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND,"존재하지 않는 주문입니다."));
+        order.isPending();
+        return order ;
+    }
+
+    public Order getCancelableOrder(Long orderId, Long userId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CoreException(ErrorType.NOT_FOUND,"존재하지 않는 주문입니다."));
+        order.validateCancelable(userId);
+        return order ;
     }
 
     public List<Order> getOrdersByUserId(Long userId) {
         return orderRepository.findByUserId(userId);
     }
 
-    public void validateCancelable(Order order, User user) {
-        if (!order.getUserId().equals(user.getId())) {
-            throw new CoreException(ErrorType.BAD_REQUEST,"본인의 주문만 취소할 수 있습니다.");
-        }
-        if (order.getStatus() == Order.Status.CANCELED) {
-            throw new CoreException(ErrorType.BAD_REQUEST,"이미 취소된 주문입니다.");
-        }
-        if (order.getStatus() == Order.Status.CONFIRMED) {
-            throw new CoreException(ErrorType.BAD_REQUEST,"결제 완료된 주문은 취소할 수 없습니다.");
-        }
-    }
-
     public void cancelOrder(Order order) {
         order.cancel();
+        orderRepository.save(order);
+    }
+
+    public void confirmOrder(Order order) {
+        order.confirm();
         orderRepository.save(order);
     }
 }
--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/controller/order/OrderV1Request.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/controller/order/OrderV1Request.java
@@ -6,20 +6,15 @@ public class OrderV1Request {
 
     public record CreateOrder(
             String loginId,
-            List<OrderItemRequest> items,
-            Long cartCouponId
+            List<OrderItemRequest> items, // 여러개의 주문을 담기위해 리스트로 선언 // 만약에 상품쿠폰있으면 여기서 처리
+            Long cartCouponId // 장바구니 쿠폰
     ) {
 
     }
     public record OrderItemRequest(
             Long productSkuId,
             int quantity,
-            Long itemCouponId
+            Long itemCouponId // 상품쿠폰
     ) {
     }
-
-    public record CancelOrder(
-            String loginId,
-            Long orderId
-    ) {}
 }
